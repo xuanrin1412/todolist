1e9e7482dbaaf7dd339fced7ed0efb69077387fe
xuanrin1412 <xuanrin1412@gmail.com>
2023-10-05 13:45:25 +0700

    fe

---
+++ FE/src/TaskForm.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function TaskForm({ onAdd }) {
+    const [taskName, setTaskName] = useState("");
+    function handleSubmit(ev) {
+        if (taskName.trim() !== "") {
+            ev.preventDefault();
+            onAdd(taskName);
+            setTaskName("");
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <button>+</button>
+            <input
+                type="text"
+                value={taskName}
+                onChange={(ev) => setTaskName(ev.target.value)}
+                placeholder="Your next task..."
+            />
+        </form>
+    );
+}
